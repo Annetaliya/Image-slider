67487dfcfed62d974fb37f8729f652048b0f925e
Annette <annetaliya@gmail.com>
2025-02-11 13:36:19 -0800

    image slider

---
+++ slider.js
@@ -0,0 +1,24 @@
+const next = document.querySelector('#btn1');
+const previous = document.querySelector('#btn2')
+const images = document.querySelectorAll('img');
+let currentIndex = 0;
+
+
+images.forEach((image, index) => {
+    if (index !== currentIndex) {
+        image.style.display ='none'
+    }
+})
+next.addEventListener('click', () => {
+    images[currentIndex].style.display = 'none';
+    currentIndex = (currentIndex + 1) % images.length
+    images[currentIndex].style.display = 'block'
+    
+   console.log(image)
+
+})
+previous.addEventListener('click', () => {
+    images[currentIndex].style.display ='none';
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+    images[currentIndex].style.direction = 'block';
+})
